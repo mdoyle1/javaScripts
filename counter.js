3840cfdcb7193610c8f1e64f105cfa8f1aad5be0
Mark Doyle Jr <35176924+mdoyle1@users.noreply.github.com>
2018-06-23 01:55:52 -0400

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,14 +1,15 @@
 var printMillisec = 0;
 var printSec = 0;
-var printMinute = 0;
+let printMinute = 0;
+var stopTime;
 
 // stop() stops the counter from priting to the console
-const stop = ()=> {clearInterval(stopwatch.printUnit)};
+const stop = ()=> {clearInterval(stopTime)};
 
 // timeInterval(unit and # of milliseconds) starts the counter using the given speed
 var unit = 0;
 
 let	stopwatch = (unit, milliseconds)=> {
 let printUnit = ()=> console.log(unit=unit+1);
-	setInterval (printUnit, milliseconds);
-return 0}
+stopTime = setInterval (printUnit, milliseconds);
+return unit}
