8e663464e50206a1b6a69f9067a49186a6bdb418
Mark Doyle <mdoyle1@gmail.com>
2018-08-05 18:52:37 -0400

    Fixed the stoppedDuration var.  The clock works as expected.

--- stopwatch.js
+++ stopwatch.js
@@ -3,6 +3,9 @@ let stoppedDuration = null;
 let started = null;
 let timeStopped = null;
 let laps = '';
+var updateTime = null;
+var constTime = null;
+
 
 function round(value, decimals) {
   return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
@@ -12,20 +15,34 @@ function start() {
 
     if (timeBegan === null) {
         timeBegan = new Date();
-    }else {
+
+    }
+    else {
     	clearInterval(started);
     };
 
+/* This finds the stopwatch stop duration by taking the current
+time and subtracting the timeStopped variable recorded with the 
+stop button
+*/
     if (timeStopped !== null) {
         stoppedDuration += (new Date() - timeStopped);
     };
     
+    /* The following logic converts the stopDuration time 
+    to print to the console nicely.  This can be made into 
+    another funcion and removed from the current function.
+    */
+
     if (stoppedDuration < 1000){
     	console.log(stoppedDuration+' ms');
     }
 
     else if (stoppedDuration < 60000){
-    	console.log(stoppedDuration/1000+' seconds');
+        let seconds = stoppedDuration/1000+' seconds';
+    	console.log(seconds);
+    
+
     }
 
     else if(stoppedDuration < 360000){
@@ -36,14 +53,16 @@ function start() {
     
     started = setInterval(clockRunning, 10);
 
-    return stoppedDuration	
-}
+return stoppedDuration
+
+};
 
 function stop() {
+    
     timeStopped = new Date();
     clearInterval(started);
     return timeStopped
-}
+};
  
 function reset() {
     clearInterval(started);
@@ -55,11 +74,12 @@ function reset() {
     for(var i=0; ul=lapReset[i]; i++) {
     ul.parentNode.removeChild(ul);
 }
-}
+};
 
 function clockRunning(){
-    var currentTime = new Date()
-        , timeElapsed = new Date(currentTime - timeBegan)
+
+     var currentTime = new Date()
+        , timeElapsed = new Date(currentTime - timeBegan - stoppedDuration)
         , hour = timeElapsed.getUTCHours()
         , min = timeElapsed.getUTCMinutes()
         , sec = timeElapsed.getUTCSeconds()
@@ -70,6 +90,7 @@ function clockRunning(){
         (min > 9 ? min : "0" + min) + ":" + 
         (sec > 9 ? sec : "0" + sec) + "." + 
         (ms > 99 ? ms : ms > 9 ? "0" + ms : "00" + ms);
+    
 };
 
 function lap(){ 
